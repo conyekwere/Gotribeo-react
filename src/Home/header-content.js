66faa17c7840af5e9b302cdbd73cd70d21bee150
Onyekwere <OnyekC@moodys.com>
2018-06-04 19:45:24 -0400

    autofill color change  cta prompt

--- src/Home/header-content.js
+++ src/Home/header-content.js
@@ -4,37 +4,54 @@ import particle from '../images/particle.svg';
 import moon from '../images/moon.svg';
 
 export class HeaderContent extends React.Component {
-    render() {
-        return (
-            <Fragment>
-                <div className="img-wrapper">
-                    <img className="globe-img" src={globe} alt="globe" />
-                    <img className="particle-img" src={particle} alt="logo" />
-                    <img className="moon-img" src={moon} alt="moon" />
-                </div>
-                <span className="header-circle">
-                </span>
-                <span className="header-circle">
-                </span>
-                <span className="header-circle">
-                </span>
-                <div className=" col-xs-9  col-sm-12 col-md-12 col-lg-10  header-container">
-                    <div className="large">
-                        <p>Join the movement
-                        </p>
-                    </div>
-                    <div className="small">
-                        <p>Gotribeo is a kickstarter for businesses, communities and people with shared goals around the world.</p>
-                    </div>
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
 
-                    <form action="#" method="post">
-                        <input type="email" name="email" id="id_email" placeholder="name@domain.com" />
-                        <button type="submit">
-                            <p>Get Started</p>
-                        </button>
-                    </form>
-                </div>
-            </Fragment>
-        );
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit = (e)  => {
+    alert('A name was submitted: ' + this.state.value);
+   {/* this.props.toggle(type);*/}
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <Fragment>
+        <div className="img-wrapper">
+          <img className="globe-img" src={globe} alt="globe" />
+          <img className="particle-img" src={particle} alt="logo" />
+          <img className="moon-img" src={moon} alt="moon" />
+        </div>
+        <span className="header-circle">
+        </span>
+        <span className="header-circle">
+        </span>
+        <span className="header-circle">
+        </span>
+        <div className=" col-xs-9  col-sm-12 col-md-12 col-lg-10  header-container">
+          <div className="large">
+            <p>Join the movement
+                        </p>
+          </div>
+          <div className="small">
+            <p>Gotribeo is a kickstarter for businesses, communities and people with shared goals around the world.</p>
+          </div>
+
+          <form onSubmit={this.handleSubmit}>
+            <input type="email" name="email" value={this.state.value} onChange={this.handleChange} placeholder="name@domain.com" />
+            <button type="submit">
+              <p>Get Started</p>
+            </button>
+          </form>
+        </div>
+      </Fragment>
+    );
+  }
 }
